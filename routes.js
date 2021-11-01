639df1c9f50b554d31646c5cb54c1b478c00cbb7
Sereysopea-Ung <87691779+Sereysopea-Ung@users.noreply.github.com>
2021-11-01 08:44:31 +0000

    change route.js req.render to res.render

--- routes.js
+++ routes.js
@@ -30,7 +30,7 @@ module.exports = function (app, myDataBase) {
      });
 
      app.route('/chat').get(ensureAuthenticated, (req,res)=>{
-       req.render(process.cwd()+'/views/pug/chat',{
+       res.render(process.cwd()+'/views/pug/chat',{
          user: req.user
        });
      });
