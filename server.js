2bce2ee4c1afb1b6336202a418055cf488c567e6
unknown <ungsereysopeacontact@gmail.com>
2021-11-06 09:22:39 +0700

    Hi

--- server.js
+++ server.js
@@ -89,12 +89,20 @@ myDB(async client=>{
   io.on('connection', socket => {
     console.log('user ' + socket.request.user.name + ' connected');
     currentUsers ++;
-    io.emit('user count', currentUsers);
+    io.emit('user', {
+      name: socket.request.user.name,
+      currentUsers,
+      connected: true
+    });
     console.log('A user has connected');
     
     socket.on('disconnect', ()=>{
       --currentUsers;
-      io.emit('user count', currentUsers);
+      io.emit('user', {
+        name: socket.request.user.name,
+        currentUsers,
+        connected: false
+      });
     })
   });
 
